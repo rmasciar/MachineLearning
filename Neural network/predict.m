9e349564a02258adfc90fc9ce21cd23fad05deff
rmasciar <roberto.masciarelli@hotmail.it>
2015-01-19 16:47:08 +0100

    first draft complete

--- Neural network/predict.m
+++ Neural network/predict.m
@@ -1,19 +1,25 @@
 function h=predict(x,theta,network)
 
+	m=size(x,1);
 	L=length(network);
 	
-	a=[1;x(:)];
-	for l=2:L
-		
-		theta_prev=getTheta(theta,network,l-1);
-		
-		a_prev=getA(a,network,l-1);
-				
-		z=theta_prev*a_prev;
-		if l==L
-			a_next=[sigmoid(z)];
-		else a_next=[1;sigmoid(z)];
+	for i=1:m
+		a=[1;x(i,:)];
+		for l=2:L
+			
+			theta_prev=getTheta(theta,network,l-1);
+			
+			a_prev=getA(a,network,l-1);
+					
+			z=theta_prev*a_prev;
+			if l==L
+				a_next=[sigmoid(z)];
+			else a_next=[1;sigmoid(z)];
+			end
+			a=[a;a_next];
 		end
-		a=[a;a_next];
+		if i==1
+			h=getA(a,network,L)';
+		else h=[h;getA(a,network,L)'];
+		end
 	end
-	h=getA(a,network,L)';
