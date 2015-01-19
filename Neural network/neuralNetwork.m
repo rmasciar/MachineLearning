5cd0a7e8a55ab2981ee1beff28d4a7c1e1d2b24a
rmasciar <roberto.masciarelli@hotmail.it>
2015-01-19 16:53:36 +0100

    first draft complete

--- Neural network/neuralNetwork.m
+++ Neural network/neuralNetwork.m
@@ -1,4 +1,4 @@
-function [optTheta,network]=neuralNetwork(y,x,hidden,lambda=0.1,MaxIter=10)
+function [optTheta,network]=neuralNetwork(y,x,hidden,lambda=0.1,MaxIter=400)
 
 	m=size(y,1);
 	K=size(y,2);
@@ -7,7 +7,7 @@ function [optTheta,network]=neuralNetwork(y,x,hidden,lambda=0.1,MaxIter=10)
 	network=[n;hidden(:);K];
 
 	% advanced optimization
-	options=optimset('GradObj','on','MaxIter','100');
+	options=optimset('GradObj','on','MaxIter',MaxIter);
 	initialTheta=initTheta(network);
 	
 	[optTheta,functionVal,exitFlag,o]=fminunc(@(theta)(neuralCost(y,x,theta,lambda,network)),initialTheta,options);
